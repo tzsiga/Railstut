1c6936be980c297bfed02465a815e97bca82d492
zsiga <zsiga@t61.(none)>
2010-12-31 13:32:22 +0100

    cleanup

--- public/stylesheets/comedy/img/widgets/galeria_lista.php
+++
@@ -1,8 +0,0 @@
-<div class="padder">
-	<h1>Előadások képei:</h1>
-	<ul id="gal_list">
-		<?php foreach ($plays as $play): ?>
-		<li><a class="bigger" href="<?= base_url() ?>index.php/galeria/eloadas/<?= $play->ID ?>"><?= $play->author ?>: <?= $play->title ?></a></li>
-		<?php endforeach ?>
-	</ul>
-</div>
--- public/stylesheets/comedy/img/widgets/hirek.php
+++
@@ -1,5 +0,0 @@
-<div class="padder">
-	<h1>Hírek</h1>
-	<div class="date">2010.12.19.</div>
-	Megújult a színház honlapja! Hamarosan frissítjük, addig szíves türelmüket kérjük.
-</div>
